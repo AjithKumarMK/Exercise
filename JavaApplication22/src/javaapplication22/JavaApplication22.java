bd6c2cbbf22560bb581c45eddde969f40b27d431
AjithKumarMK <ajithkumarajithkumar2254@gmail.com>
2024-07-27 20:19:58 +0530

    java code

---
+++ JavaApplication22/src/javaapplication22/JavaApplication22.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication22;
+
+/**
+ *
+ * @author Ajith18
+ */
+public class JavaApplication22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 18 ;
+        if(a>18){
+            System.out.println("you are elligible");
+        }
+        else{
+            System.out.println("you are not elligible");
+        }
+                
+    }
+    
+}
